3eb93cbfc41c05db9bed3cc66876e57c89f59372
Piotr Wolny <pwolny@e-point.pl>
2018-01-03 16:29:48 +0100

    Initial commit.

---
+++ src/main/java/com/github/gildur/craftsmanship/craftsmanship/CraftsmanshipApplication.java
@@ -0,0 +1,12 @@
+package com.github.gildur.craftsmanship.craftsmanship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftsmanshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftsmanshipApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/gildur/craftsmanship/craftsmanship/CraftsmanshipApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.gildur.craftsmanship.craftsmanship;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CraftsmanshipApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
